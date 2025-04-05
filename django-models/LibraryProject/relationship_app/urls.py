f583aaf6da4f498f723aabceb78b385069cac314
Caroline-Wacera <carolinewacera4@gmail.com>
2025-04-05 12:03:33 +0300

    Added URL Routes For Books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('register/', views.register name='register'),
     path('logout/', LogoutView.as_view(template_name="logout_view")),
     path('login,', LoginView.as_view(template_name="login_view")),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
