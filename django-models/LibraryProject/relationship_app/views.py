67ae1b29c210d913df6e7baa4e81dab4b2b6afb4
Caroline-Wacera <carolinewacera4@gmail.com>
2025-04-05 11:31:54 +0300

    Added Auth Imports

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
 from .forms import BookForm  # Ensure you have a BookForm for adding/editing
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required
 @permission_required('relationship_app.can_add_book', raise_exception=True)
