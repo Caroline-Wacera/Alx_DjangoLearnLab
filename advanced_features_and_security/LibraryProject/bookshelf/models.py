890a343e1c6ba9bdd7359f1e346b493ca122ab63
Caroline-Wacera <carolinewacera4@gmail.com>
2025-04-05 12:17:02 +0300

    Added User Model Extension

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Additional fields for the custom user model
+    phone_number = models.CharField(max_length=15, unique=True)
+    address = models.TextField()
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
