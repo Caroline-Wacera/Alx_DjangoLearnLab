ac6c1bc7d48d1c41cde6bc5d3e46ed0ef2081d84
Caroline-Wacera <carolinewacera4@gmail.com>
2025-04-05 11:43:10 +0300

    Added Auth Views Import

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import admin_view, librarian_view, member_view, register
+import views
 from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-    path('register/', register name='register'),
+    path('register/', views.register name='register'),
     path('logout/', LogoutView.as_view(template_name="logout_view")),
     path('login,', LoginView.as_view(template_name="login_view")),
 ]
