45274a27ebdb29781795d109815c04c9ad882808
Caroline-Wacera <carolinewacera4@gmail.com>
2025-04-05 12:51:30 +0300

    Added Form

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter book title'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter author name'}),
+            'publication_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter publication year'}),
+        }
+        labels = {
+            'title': 'Book Title',
+            'author': 'Author Name',
+            'publication_year': 'Publication Year',
+        }
+        help_texts = {
+            'title': 'Enter the title of the book.',
+            'author': 'Enter the name of the author.',
+            'publication_year': 'Enter the year the book was published.',
+        }
+        error_messages = {
+            'title': {
+                'required': 'This field is required.',
+            },
+            'author': {
+                'required': 'This field is required.',
+            },
+            'publication_year': {
+                'required': 'This field is required.',
+                'invalid': 'Enter a valid year.',
+            },
+        }
